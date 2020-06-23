9c1b451ec7738d499b1bf304a8b95889d1168f49
Oscar Lara <me@oscarlara.dev>
2020-06-22 23:42:39 -0500

    Se modifica el constructor de la clase Doctor

--- src/App.java
+++ src/App.java
@@ -2,6 +2,9 @@ import static ui.UIMenu.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        showMenu();
+        //showMenu();
+        Doctor myDoctor = new Doctor("Oscar Lara", "Neurología");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
     }
 }
--- src/Doctor.java
+++ src/Doctor.java
@@ -4,8 +4,14 @@ public class Doctor {
     String speciality;
 
     Doctor() {
+        System.out.println("Construyendo el objeto doctor");
+    }
+
+    Doctor(String name, String speciality) {
         System.out.println("Construyendo el objeto Doctor");
         id++;
+        this.name = name;
+        this.speciality = speciality;
     }
 
     public void showName() {
