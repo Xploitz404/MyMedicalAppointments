71cca1127e7ba8de0aed6f551009142bea119afd
Oscar Lara <me@oscarlara.dev>
2020-06-22 23:33:08 -0500

    Se crea la clase Patient

---
+++ src/Patient.java
@@ -0,0 +1,14 @@
+public class Patient {
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    String blood;
+    double weight;
+    double height;
+    Patient(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
