b568330b7ca8208046f870cd896825b2a848d8c8
Oscar Lara <me@oscarlara.dev>
2020-06-22 21:57:10 -0500

    Se crea la clase UIMenu

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
+import static ui.UIMenu.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+        Doctor.id++;
+        Doctor myDoctorOscar = new Doctor();
+        myDoctor.showId();
+        showMenu();
     }
 }
--- src/Doctor.java
+++ src/Doctor.java
@@ -1,13 +1,18 @@
 public class Doctor {
-    int id;
+    static int id = 0; // Autoincrement
     String name;
     String speciality;
 
-    Doctor(String name) {
-        System.out.println("El nombre del doctor asignado es: " + name);
+    Doctor() {
+        System.out.println("Construyendo el objeto Doctor");
+        id++;
     }
 
     public void showName() {
         System.out.println(name);
+    }
+
+    public void showId() {
+        System.out.println("ID Doctor: " + id);
     }
 }
