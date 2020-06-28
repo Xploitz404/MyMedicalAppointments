3f3bb7ce17289154a9b562967ff8a73078aaa4d8
Oscar Lara <me@oscarlara.dev>
2020-06-28 18:41:18 -0500

    Se crea la clase AvailableAppointments

--- src/App.java
+++ src/App.java
@@ -1,11 +1,17 @@
 import static ui.UIMenu.*;
+import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
         //showMenu();
         Doctor myDoctor = new Doctor("Oscar Lara", "Neurología");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
+
+        for (Doctor.AvailableAppointment availableAppointment : myDoctor.getAvailableAppointments()) {
+            System.out.println(availableAppointment.getDate() + " " + availableAppointment.getTime());
+        }
 
         Patient patient = new Patient("Alejandra", "ale@gmail.com");
         patient.setWeight(62.5);
--- src/Doctor.java
+++ src/Doctor.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
-    static int id = 0; // Autoincrement
-    String name;
-    String speciality;
+    private static int id = 0; // Autoincrement
+    private String name;
+    private String speciality;
 
     Doctor() {
         System.out.println("Construyendo el objeto doctor");
     }
 
     Doctor(String name, String speciality) {
-        System.out.println("Construyendo el objeto Doctor");
+        System.out.println("El nombre del doctor asignado es: " + name);
         id++;
         this.name = name;
         this.speciality = speciality;
@@ -20,5 +23,65 @@ public class Doctor {
 
     public void showId() {
         System.out.println("ID Doctor: " + id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public static class AvailableAppointment {
+        private int id_availableAppointment;
+        private Date date;
+        private String time;
+
+        AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId_availableAppointment() {
+            return id_availableAppointment;
+        }
+
+        public void setId_availableAppointment(int id_availableAppointment) {
+            this.id_availableAppointment = id_availableAppointment;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
