609e25400765010ad97c32454e01836087ac0f09
Oscar Lara <me@oscarlara.dev>
2020-07-06 11:12:57 -0500

    Simulacion de autenticacion

--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -1,11 +1,20 @@
 package ui;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.print.Doc;
+
+import model.Doctor;
+import model.Patient;
 
 public class UIMenu {
 
     public static final String[] MONTHS= {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+    public static Doctor doctorLogged;
+    public static Patient patientLogged;
 
+    
     public static void showMenu(){
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opción deseada");
@@ -22,11 +31,12 @@ public class UIMenu {
             switch (response){
                 case 1:
                     System.out.println("Doctor");
+                    response = 0;
+                    authUser(1);
                     break;
                 case 2:
                     response = 0;
-                    showPatientMenu();
-
+                    authUser(2);
                     break;
                 case 0:
                     System.out.println("Thank you for you visit");
@@ -35,6 +45,46 @@ public class UIMenu {
                     System.out.println("Please select a correct answer");
             }
         }while (response != 0);
+    }
+
+    private static void authUser(int userType){
+        //userType = 1: Doctor
+        //userType = 2: Patient
+
+        ArrayList<Doctor> doctors = new ArrayList<>();
+        doctors.add(new Doctor("Bojack Horseman", "bojack@netflix.com"));
+        doctors.add(new Doctor("Mr. Peanut Butter", "peanutButter@netflix.com"));
+        doctors.add(new Doctor("Princesa Caroline", "princessCaroline@netflix.com"));
+
+        ArrayList<Patient> patients = new ArrayList<>();
+        patients.add(new Patient("Todd Sánchez", "tooood@netflix.com"));
+        patients.add(new Patient("Diane Peanut Butter", "tooood@netflix.com"));
+        patients.add(new Patient("Herb Kazzas", "Herb@netflix.com"));
+
+        boolean emailCorrect = false;
+        do {
+            System.out.println("Insert your email: [example@mail.com]");
+            Scanner sc = new Scanner(System.in);
+            String email = sc.nextLine();
+            if (userType == 1) {
+                for (Doctor d: doctors) {
+                    if (d.getEmail().equals(email)) {
+                        //Obtener el usuario logueado
+                        emailCorrect = true;
+                        doctorLogged = d;
+                        //showDoctorMenu
+                    }
+                }
+            } else if(userType == 2) {
+                for (Patient p : patients) {
+                    if (p.getEmail().equals(email)) {
+                        emailCorrect = true;
+                        patientLogged = p;
+                        // showPatientMenu
+                    }
+                }
+            }
+        } while (!emailCorrect);
     }
 
     public static void showPatientMenu(){
